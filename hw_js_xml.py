c61d3d64ed57e1faaef84bcabb082f6ab4f87ea0
IliKet <Nika-katena@yandex.ru>
2024-04-09 20:05:50 +1100

    Update HomeWork

--- hw_js_xml.py
+++ hw_js_xml.py
@@ -12,7 +12,11 @@ for news in news_list:
   for word in news.text.lower().split():
       if len(word) > 6:
             words.append(word)
-print(Counter(words).most_common(10))
+words_freq_count = Counter(words).most_common(10)
+words_freq = []
+for word_freq in words_freq_count:
+    words_freq.append(word_freq[0])
+print(words_freq)
 
 
 with open("newsafr.json", encoding="utf-8") as f:
@@ -22,7 +26,11 @@ news_list = json_data["rss"]["channel"]["items"]
 description = [news['description'] for news in news_list]
 words = []
 for news in description:
-    for word in news.lower().split():
+  for word in news.lower().split():
       if len(word) > 6:
             words.append(word)
-print(Counter(words).most_common(10))
+words_freq_count = Counter(words).most_common(10)
+words_freq = []
+for word_freq in words_freq_count:
+    words_freq.append(word_freq[0])
+print(words_freq)
