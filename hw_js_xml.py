98edc37516749b7fc6871881ac6a725554bfd784
IliKet <Nika-katena@yandex.ru>
2024-04-09 19:16:19 +1100

    HW_Json_Xml

---
+++ hw_js_xml.py
@@ -0,0 +1,28 @@
+import xml.etree.ElementTree as ET
+import json
+from collections import Counter
+
+parser = ET.XMLParser(encoding="utf-8")
+tree = ET.parse("newsafr.xml", parser)
+root = tree.getroot()
+
+news_list = root.findall('channel/item/description')
+words = []
+for news in news_list:
+  for word in news.text.lower().split():
+      if len(word) > 6:
+            words.append(word)
+print(Counter(words).most_common(10))
+
+
+with open("newsafr.json", encoding="utf-8") as f:
+    json_data = json.load(f)
+
+news_list = json_data["rss"]["channel"]["items"]
+description = [news['description'] for news in news_list]
+words = []
+for news in description:
+    for word in news.lower().split():
+      if len(word) > 6:
+            words.append(word)
+print(Counter(words).most_common(10))
